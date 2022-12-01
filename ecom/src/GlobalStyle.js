6aecc65fb3e43e3a76c8af045bff2b0d22bd10e6
Dawa <sherpadawa01@outlook.com>
2022-12-02 00:30:28 +0545

    custom theme done

--- ecom/src/GlobalStyle.js
+++ ecom/src/GlobalStyle.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 
@@ -82,7 +82,7 @@ li {
 }
 
 
-${"" /* resuable code section  */}
+${'' /* resuable code section  */}
 
 .container {
   max-width: 120rem;
@@ -157,7 +157,7 @@ input, textarea{
     cursor: pointer;
     }
 
-@media (max-width: ${({ theme }) => theme.media.tab}) {
+   @media (max-width: ${({ theme }) => theme.media.tab}) {
     .container {
     max-width: 130rem;
     padding: 0 3.2rem;
@@ -168,6 +168,7 @@ input, textarea{
        html {
       font-size: 50%;
     }
+
 
 .grid{
   gap: 3.2rem;
